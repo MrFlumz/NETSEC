7f99b5bd99d6c10e05a2fa44f649bd1292089561
mrflumz <flumz26@gmail.com>
2021-11-01 17:53:33 +0100

    print server data

--- Ass2/2. Throttling TCP connections/server.py
+++ Ass2/2. Throttling TCP connections/server.py
@@ -2,11 +2,13 @@ import socket
 
 HOST = '192.168.43.112'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+
 while True:
 	try:
 		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 			s.bind((HOST, PORT))
-			s.settimeout(3)
+			s.settimeout(5)
 			print("listening on "+str(HOST)+":"+str(PORT))
 			s.listen()
 			conn, addr = s.accept()
@@ -14,6 +16,7 @@ while True:
 				print('Connected by', addr)
 				while True:
 					data = conn.recv(1024)
+					print(data)
 					if not data:
 						break
 					conn.sendall(data)
